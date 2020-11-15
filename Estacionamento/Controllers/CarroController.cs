fd503475bf591cd33a50ec5150d644b5cbacee30
regis.numata <regis.numata@gmail.com>
2020-11-15 12:46:47 -0300

    Criado CarroController, a View Carro/Cadastrar e inserido o action no Navbar

--- Estacionamento/Controllers/CarroController.cs
+++ Estacionamento/Controllers/CarroController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Estacionamento.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Estacionamento.Controllers
 {
     public class CarroController : Controller
     {
-        public IActionResult Index()
+
+        public IActionResult Cadastrar()
         {
+            ViewBag.Title = "Cadastro Carro";
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Carro Carro)
+        {
+            if (ModelState.IsValid)
+            {
+                //incluir cadastro no DAO
+                return RedirectToAction("Index", "Estacionamento");
+            }
+            ModelState.AddModelError("", "Cadastro não realizado");
+            return View();
+        }
+
     }
 }
