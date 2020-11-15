73cf2b5e98f4d5726e6003b2c70d4a3d11e6a868
regis.numata <regis.numata@gmail.com>
2020-11-15 12:02:19 -0300

    Criada Classe Context, atualizado Startup.cs e appSettings.json

---
+++ Estacionamento/Models/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Carro> Carros { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Estacionamento> Estacionamento { get; set; }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public DbSet<Movimentacao> Movimentacao { get; set; }
+
+        public DbSet<Vaga> Vagas { get; set; }
+
+
+    }
+}
--- Estacionamento/Startup.cs
+++ Estacionamento/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Estacionamento.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,13 @@ namespace Estacionamento
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Inserir os DAOs
+
+
+            //Definicao do BD e string de conexao
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Connection")));
+
+
             services.AddControllersWithViews();
         }
 
