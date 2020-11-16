c1d616e1c4424429258a29faa4595faaf73058be
Jaime Linhares <jaimelinharesjr@gmail.com>
2020-11-16 19:22:52 -0300

    Criado primeira migração.

---
+++ Estacionamento/Migrations/20201116222124_CriarBase.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estacionamento.Migrations
+{
+    public partial class CriarBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Nome = table.Column<string>(nullable: false),
+                    CPF = table.Column<string>(nullable: false),
+                    Telefone = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estacionamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Vagas = table.Column<int>(nullable: false),
+                    Diaria = table.Column<double>(nullable: false),
+                    Horista = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estacionamento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Nome = table.Column<string>(nullable: false),
+                    CPF = table.Column<string>(nullable: false),
+                    Funcao = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vagas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vagas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Marca = table.Column<string>(nullable: false),
+                    Modelo = table.Column<string>(nullable: false),
+                    Cor = table.Column<string>(nullable: false),
+                    Placa = table.Column<string>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carros_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movimentacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CriadoEm = table.Column<DateTime>(nullable: false),
+                    Entrada = table.Column<DateTime>(nullable: false),
+                    Saida = table.Column<DateTime>(nullable: false),
+                    CarroId = table.Column<int>(nullable: true),
+                    VagaId = table.Column<int>(nullable: true),
+                    Total = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimentacao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movimentacao_Carros_CarroId",
+                        column: x => x.CarroId,
+                        principalTable: "Carros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Movimentacao_Vagas_VagaId",
+                        column: x => x.VagaId,
+                        principalTable: "Vagas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carros_ClienteId",
+                table: "Carros",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimentacao_CarroId",
+                table: "Movimentacao",
+                column: "CarroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimentacao_VagaId",
+                table: "Movimentacao",
+                column: "VagaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estacionamento");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+
+            migrationBuilder.DropTable(
+                name: "Movimentacao");
+
+            migrationBuilder.DropTable(
+                name: "Carros");
+
+            migrationBuilder.DropTable(
+                name: "Vagas");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
