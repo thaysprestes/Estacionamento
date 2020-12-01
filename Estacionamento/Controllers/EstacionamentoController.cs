099ac8d9fb02ed1867bb8f304de192907b917ae9
regis.numata <regis.numata@gmail.com>
2020-12-01 16:29:25 -0300

    View Index com carros estacionados

--- Estacionamento/Controllers/EstacionamentoController.cs
+++ Estacionamento/Controllers/EstacionamentoController.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Estacionamento.DAL;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Estacionamento.Controllers
 {
     public class EstacionamentoController : Controller
     {
+
+        private readonly CarroDAO _carroDAO;
+
+        public EstacionamentoController(CarroDAO carroDAO)
+        {
+            _carroDAO = carroDAO;
+        }
+
+
+
         public IActionResult Index()
         {
             ViewBag.Title = "Estacionamento JRT";
-            return View();
+            return View(_carroDAO.Listar());
         }
 
         public IActionResult Configurar()
--- Estacionamento/wwwroot/js/site.js
+++ Estacionamento/wwwroot/js/site.js
@@ -14,6 +14,6 @@ function buscaBitcoin() {
 
 setInterval(() => {
     buscaBitcoin();
-}, 10000);
+}, 50000);
 
 
